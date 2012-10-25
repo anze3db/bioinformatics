c1e09b83ef6ac17cca346f9c8ff507bf8bdc8091
Smotko <ensmotko@gmail.com>
2012-10-25 22:06:50 +0200

    Almost done, report and some code refactoring left

--- src/homework1/main.py
+++ src/homework1/main.py
@@ -8,51 +8,36 @@ def get_seq():
 
     f = open("NC_001416.1.fasta")
     fc = f.read()
-    fc = fc.replace('N', '') # TODO: figure out how to handle N (any nucleic acid)
+    fc = fc.replace('N', '')
     return list("".join(fc.split("\n")[1:]))
 
-def print_probability(cn):
+def print_probability(cn, N):
     """Print ordered probabilities"""
     
-    l = list(cn)
-    l.sort(key=lambda c: cn.get(c), reverse=True) # Sort by number of occurrences
     s = ""
-    for c in l:
-        s += "%s - %0.2f%%, " % (c, cn.get(c) * 100 / float(sum(cn.values())))
-    print s + "\n"
+    for c in sorted(list(cn), key=lambda c: cn[1], reverse=True):
+        s += "%s - %0.2f%%, " % (c[0], c[1] * 100 / float(N))
+    print s[:-2] + "\n"
     
 def report_changes(seq, window_size):
     """Report changes of the aggregate frequency of G or C"""
     
-    window_frequence = []
+    return [len([j for j in seq[i:i+window_size] if j == "C" or j == "G"]) for i,_ in enumerate(seq) if i % 50 == 0]
     
-    c = 0
-    cnt = 0
-    for s in seq:
-        cnt += 1
-        if s == "C" or s == "G":
-            c += 1
-            
-        if cnt == window_size:
-            window_frequence.append(c)
-            cnt = 0
-            c   = 0
-            
-    return window_frequence
+def graph(window_frequence):     
+    pylab.plot(range(len(window_frequence)), window_frequence, lw=1)
     
-def graph(window_frequence):        
-    #pylab.bar(range(len(window_frequence)), window_frequence)
-    pass
 def cgr(seq):
+    
     m = {'A': 0, 'T': 1, 'C': 0, 'G': 1}
     n = {'A': 0, 'C': 1, 'T': 0, 'G': 1}
-    a,b = 0,0
+    a, b = 0, 0
     k = len(seq)
     for i in range(k):
-        a += m[seq[i]] * 2**(k-i-1)
-        b += n[seq[i]] * 2**(k-i-1)
+        a += m[seq[i]] * 2 ** (k - i - 1)
+        b += n[seq[i]] * 2 ** (k - i - 1)
     
-    return a,b
+    return a, b
 
 if __name__ == '__main__':
     
@@ -61,16 +46,17 @@ if __name__ == '__main__':
     
     # Calculate occurrences for 1-mers and 2-mers:
     c1 = Counter(seq)
-    c2 = Counter([l1 + l2 for l1, l2 in zip(seq[:-1], seq[1:])])    
+    c2 = Counter(l1 + l2 for l1, l2 in zip(seq[:-1], seq[1:]))
 
-    print_probability(c1)
-    print_probability(c2)
+    print_probability(c1.items(), N)
+    print_probability(c2.most_common(10), N)
 
     # Calculate deviations:
     ls = []
+
     for c in c2:
         l = list(c)
-        ls.append((c, log(N * c2.get(c) / float(c1[l[0]] * c1[l[1]]),2)))
+        ls.append((c, log(N * c2.get(c) / float(c1[l[0]] * c1[l[1]]), 2)))
 
     ls.sort(key=lambda c: c[1], reverse=True)
     s = ""
@@ -78,24 +64,48 @@ if __name__ == '__main__':
         s += "%s - %0.4f, " % (c[0], c[1])
     print s + "\n"
     
-    #Report changes on aggregate frequencies:
+    # Report changes on aggregate frequencies:
     
-    [graph(report_changes(seq, i)) for i in [10,100,1000]]
-    # pylab.show() # TODO: Normalize
+    [graph(report_changes(seq, i)) for i in [10, 100, 1000]]
     
+    pylab.yscale('log')
+    pylab.xlabel("Genome sequence")
+    pylab.ylabel("G or C frequency")
+    pylab.title("GC content across the genome")
+    
+    pylab.show()
     # Fill up CGR table:
     seq.reverse()
     k = 4
     
+    pylab.figure()
+    pylab.subplot(121, aspect='equal')
+    pylab.title("CGR (k = 4)")
+    
     cgra = []
-    for i in range(16):
+    for i in range(2**k):
         cgra.append([])
-        for _ in range(16):
+        for _ in range(2**k):
             cgra[i].append(0)
             
-    for i in range(0, len(seq)-k+1):
-        a,b = cgr(seq[i:i+k])
+    for i in range(0, len(seq) - k + 1):
+        a, b = cgr(seq[i:i + k])
         cgra[a][b] += 1
     
-    pylab.imshow(cgra, interpolation='nearest', cmap = cm.gray)
+    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray)
+    k = 3
+    pylab.subplot(122, aspect='equal')
+    pylab.title("CGR (k = 3)")
+    
+    cgra = []
+    for i in range(2**k):
+        cgra.append([])
+        for _ in range(2**k):
+            cgra[i].append(0)
+            
+    for i in range(0, len(seq) - k + 1):
+        a, b = cgr(seq[i:i + k])
+        cgra[a][b] += 1
+    
+    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray)
     pylab.show()

1057ef84a9294d326e02b916713e002946134aa5
Smotko <ensmotko@gmail.com>
2012-10-26 02:16:44 +0200

    Homework 1 finished, need to do the additional one as well

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 shopt -s extglob
 rm pecar-anze.zip
-zip -j pecar-anze.zip src/homework$1/!(_*) rpt/homework$1/pecar-anze.pdf
+zip -j pecar-anze.zip src/homework$1/!(_*|*.pyc) rpt/homework$1/pecar-anze.pdf
--- src/homework1/main.py
+++ src/homework1/main.py
@@ -1,14 +1,14 @@
-import pylab
 from collections import Counter
 from math import log
 from matplotlib import cm
+import pylab
 
 def get_seq():
     """Return  mitochondrial sequences in FASTA format"""
 
     f = open("NC_001416.1.fasta")
     fc = f.read()
-    fc = fc.replace('N', '')
+    fc = fc.replace('N', '')  # ignore aNy
     return list("".join(fc.split("\n")[1:]))
 
 def print_probability(cn, N):
@@ -20,14 +20,18 @@ def print_probability(cn, N):
     print s[:-2] + "\n"
     
 def report_changes(seq, window_size):
-    """Report changes of the aggregate frequency of G or C"""
+    """Report changes of aggregate frequency of G or C"""
     
-    return [len([j for j in seq[i:i+window_size] if j == "C" or j == "G"]) for i,_ in enumerate(seq) if i % 50 == 0]
+    return [len([j for j in seq[i:i + window_size] if j == "C" or j == "G"]) for i, _ in enumerate(seq) if i % 50 == 0]
     
-def graph(window_frequence):     
+def graph(window_frequence):   
+    """Plot a graph"""
+      
     pylab.plot(range(len(window_frequence)), window_frequence, lw=1)
     
 def cgr(seq):
+    """Calculate the position in the cgr grid for the given k-mer seq"""
+    
     
     m = {'A': 0, 'T': 1, 'C': 0, 'G': 1}
     n = {'A': 0, 'C': 1, 'T': 0, 'G': 1}
@@ -38,6 +42,19 @@ def cgr(seq):
         b += n[seq[i]] * 2 ** (k - i - 1)
     
     return a, b
+
+
+def get_cgra(k):
+    cgra = []
+    for i in range(2 ** k):
+        cgra.append([])
+        for _ in range(2 ** k):
+            cgra[i].append(0)
+            
+    for i in range(0, len(seq) - k + 1):
+        a, b = cgr(seq[i:i + k])
+        cgra[a][b] += 1
+    return cgra
 
 if __name__ == '__main__':
     
@@ -56,7 +73,7 @@ if __name__ == '__main__':
 
     for c in c2:
         l = list(c)
-        ls.append((c, log(N * c2.get(c) / float(c1[l[0]] * c1[l[1]]), 2)))
+        ls.append((c, abs(log(N * c2.get(c) / float(c1[l[0]] * c1[l[1]]), 2))))
 
     ls.sort(key=lambda c: c[1], reverse=True)
     s = ""
@@ -82,30 +99,14 @@ if __name__ == '__main__':
     pylab.subplot(121, aspect='equal')
     pylab.title("CGR (k = 4)")
     
-    cgra = []
-    for i in range(2**k):
-        cgra.append([])
-        for _ in range(2**k):
-            cgra[i].append(0)
-            
-    for i in range(0, len(seq) - k + 1):
-        a, b = cgr(seq[i:i + k])
-        cgra[a][b] += 1
+    cgra = get_cgra(k)
     
-    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray)
+    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray_r)
     k = 3
     pylab.subplot(122, aspect='equal')
     pylab.title("CGR (k = 3)")
     
-    cgra = []
-    for i in range(2**k):
-        cgra.append([])
-        for _ in range(2**k):
-            cgra[i].append(0)
-            
-    for i in range(0, len(seq) - k + 1):
-        a, b = cgr(seq[i:i + k])
-        cgra[a][b] += 1
+    cgra = get_cgra(k)
     
-    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray)
+    pylab.imshow(cgra, interpolation='nearest', cmap=cm.gray_r)
     pylab.show()

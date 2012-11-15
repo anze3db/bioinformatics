e523cccebb15894ee7089f71082c8f0beaad2589
Smotko <ensmotko@gmail.com>
2012-11-15 12:34:30 +0100

    Weird numbers

--- src/homework2/main.py
+++ src/homework2/main.py
@@ -23,23 +23,26 @@ if __name__ == "__main__":
     seq = get_seq('NC_006058.1.fasta')
     ris = rev_inv(seq)
     
-    stop_codon = {"TGA"}
+    stop_codon  = {"TGA"}
     start_codon = {"ATG"}
     # Paramecium tetraurelia. Start codon: ATG. Stop codon: TGA.
     # Emiliania huxleyi virus 86. Start codons: ATG, TTG, CTG. Stop codons: TAA, TAG, TGA.
     orf_start = False
     orfs = []
+    reverse = False
     for s in (seq, ris):
         for frame in range(3):
             for i, codon in codon_walk(s, frame):
                 if not orf_start and codon in start_codon:
                     orf_start = i
                 if orf_start and codon in stop_codon:
-                    orfs.append(s[orf_start:i+3])
+                    if reverse:
+                        orfs.append({"strand": seq[len(s)-(i+3):len(s)-orf_start], "start": len(s)-(i+3), "end": len(s)-orf_start, "reverse": reverse })
+                    else:
+                        orfs.append({"strand": s[orf_start:i+3], "start":orf_start, "end": i+3, "reverse": reverse })
                     orf_start = False
-    
-    print "%s ORFs code for at least 60 amino acids" % sum(1 for g in orfs if len(g)>=1560)
-    
+        reverse = True
+    print "%s ORF codes with at least 60 amino acids" % sum(1 for g in orfs if len(g["strand"])>=60)
     
 #    from Bio import Entrez
 #    from Bio import SeqIO
@@ -49,15 +52,24 @@ if __name__ == "__main__":
 #    
 #    dump(rec, file('NC_006058.pickle', 'w'))
     rec = load(file('NC_006058.pickle'))
+    match = []
+    for o in orfs:
+        for f in rec.features:
+            if f.type == "CDS":
+                if o["start"] == f.location.start.position and o["end"] == f.location.end.position and o["reverse"] == [True, False][(f.strand+1)/2]:
+                    match.append({"calculated": o["strand"], "original": seq[f.location.start.position:f.location.end.position]})
+                continue
+                print len(seq[f.location.start.position:f.location.end.position]), seq[f.location.start.position:f.location.end.position]
+                print "start", f.location.start.position
+                print "end", f.location.end.position
+                print "strand", ["-", "+" ][(f.strand+1)/2]
+                print
     
-    for f in rec.features:
-        if f.type == "CDS":
-            continue
-            print len(seq[f.location.start.position:f.location.end.position]), seq[f.location.start.position:f.location.end.position]
-            print "start", f.location.start.position
-            print "end", f.location.end.position
-            print "strand", ["-", "+" ][(f.strand+1)/2]
-            print
-    
-    
-    
+    min_len = 9999999999
+    for m in match:
+        if len(m["original"]) < min_len:
+            min_len = len(m["original"])
+        print "or", m["original"]
+        print "ca", m["calculated"]
+    print "matched: ", len(match), "60>", sum(1 for g in orfs if len(g["strand"])>=60) 
+    print min_len, float(min_len)/sum(1 for g in orfs if len(g["strand"])>=280), float(min_len)/sum(1 for g in orfs if len(g["strand"])>=60) 

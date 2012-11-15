e612bd490d796f81da7319c861370e221450c5a6
Smotko <ensmotko@gmail.com>
2012-11-15 20:26:17 +0100

    Almost finished with report, cleaned up the code a bit

--- src/homework2/main.py
+++ src/homework2/main.py
@@ -2,33 +2,27 @@ from cPickle import dump, load
 import pylab
 
 def get_seq(fa):
-    """Return  mitochondrial sequences in FASTA format"""
+    """Return sequences in FASTA format"""
 
     f = open(fa)
     fc = f.read()
     fc = fc.replace('N', '')  # ignore aNy
     return "".join(fc.split("\n")[1:])
 
-def get_precision():
-    """Compute precision"""
-    return 
-def get_recall():
-    """Compute recall"""
-    return
-
 def rev_inv(seq):
-    """Reverses and invertise a sequence"""
+    """Reverses and complement a sequence"""
     
     s = {'A': 'T', 'T':'A', 'C': 'G', 'G': 'C'}
     return "".join([s[i] for i in seq[::-1]])
 
 def codon_walk(seq, offset, length=3):
-    """"""
+    """Walk through all possible windows"""
     for i in xrange(offset, len(seq) - (len(seq) - offset) % length, length):
         yield i, seq[i:i + length]
 
 
 def load_entrez(genome):
+    """Reads Entrez data from a .pickle file. If the file does not exist it is created"""
     import os.path
     if not os.path.isfile(genome + '.pickle'):
         from Bio import Entrez
@@ -43,19 +37,26 @@ def load_entrez(genome):
 
 if __name__ == "__main__":
     
-    genome = 'NC_007346'
-    #genome = 'NC_006058'
-    seq = get_seq(genome+'.1.fasta')
-    ris = rev_inv(seq)
+    genome = 'NC_006058'
+    #genome = 'NC_007346'
     
+    seq = get_seq(genome+'.1.fasta')
+    
+    ris = rev_inv(seq)
+
+    # Paramecium tetraurelia. Start codon: ATG. Stop codon: TGA.
+
     stop_codon = {"TGA"}
     start_codon = {"ATG"}
-    # Paramecium tetraurelia. Start codon: ATG. Stop codon: TGA.
-    # Emiliania huxleyi virus 86. Start codons: ATG, TTG, CTG. Stop codons: TAA, TAG, TGA.
-    orf_start = False
+    
+    if genome == 'NC_007346':
+        # Emiliania huxleyi virus 86. Start codons: ATG, TTG, CTG. Stop codons: TAA, TAG, TGA.
+        stop_codon = {"TAA", "TAG", "TGA"}
+        start_codon = {"ATG", "TTG", "CTG"}
+        
+    orf_start = False 
     orfs = []
     reverse = False
-    
     for s in (seq, ris):
         for frame in range(3):
             orf_start = False
@@ -64,13 +65,14 @@ if __name__ == "__main__":
                     orf_start = i
                 if orf_start and codon in stop_codon:
                     if reverse:
+                        # We need to get the strand from the original sequence:
                         orfs.append({"strand": seq[len(s) - (i + 3):len(s) - orf_start], "start": len(s) - (i + 3), "end": len(s) - orf_start, "reverse": reverse })
                     else:
                         orfs.append({"strand": s[orf_start:i + 3], "start":orf_start, "end": i + 3, "reverse": reverse })
                     orf_start = False
         reverse = True
-    print "%s without filtering" % len(orfs)
-    print "%s ORFs code for at least 60 amino acids" % sum(1 for g in orfs if len(g["strand"]) > 180)
+        
+    print "%s ORFs code for at least 60 amino acids" % sum(1 for g in orfs if len(g["strand"]) > 60*3)
     
     rec = load_entrez(genome)
     original = set(f.location.start.position for f in rec.features if f.type == "CDS")
@@ -91,10 +93,10 @@ if __name__ == "__main__":
     pylab.title("Precision and recall as a function of minimum ORF length")
     pylab.show()
 
-    # PREC & RECALL FOR 125 codons:
-    scale = 125
-    predictions = set(g['start'] for g in orfs if len(g['strand']) > scale * 3)
+
+    # Calculate precision and recall for 125 codons or more
+    c = 125
+    predictions = set(g['start'] for g in orfs if len(g['strand']) > c * 3)
     TP = len(predictions.intersection(original))
-    print "%.4f precision, %.4f recall " % ((float(TP) / len(predictions)), (float(TP) / (TP + len(original.difference(predictions)))))
-    
-    
+    print "%.4f precision, %.4f recall for 125 codons or more " \
+        % ((float(TP) / len(predictions)), (float(TP) / (TP + len(original.difference(predictions)))))
